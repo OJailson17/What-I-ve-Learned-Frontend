9052938adcc1f12bd55ed44fdd8fcc90f9121d4f
OJailson17 <JaylsonO17@gmail.com>
2021-06-10 15:13:18 -0300

    create login page

---
+++ src/pages/Auth/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Form from "../../../components/Form/Form";
+import PageTitle from "../../../components/PageTitle/PageTitle";
+
+const styles = {
+  width: "100%",
+  height: "100%",
+};
+
+function Login({ theme }) {
+  return (
+    <main style={styles}>
+      <PageTitle title="Sign In" />
+      <Form
+        buttonText="Login"
+        hiddenInput={true}
+        hiddenCheck={false}
+        helperText="Sign Up"
+        linkPage="/signup"
+        theme={theme}
+      />
+    </main>
+  );
+}
+
+export default Login;
