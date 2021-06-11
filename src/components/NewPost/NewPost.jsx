a087fa7104aee111fc2e80be4dcb07476cf7549f
OJailson17 <JaylsonO17@gmail.com>
2021-06-11 02:19:30 -0300

    create newPost component

---
+++ src/components/NewPost/NewPost.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import AddIcon from "@material-ui/icons/Add";
+import { Link } from "react-router-dom";
+
+const blue = "#025ceb";
+
+const buttonStyles = {
+  border: `1px solid ${blue}`,
+  borderRadius: "5px",
+  width: "100px",
+  height: "30px",
+  background: "transparent",
+  fontSize: "14px",
+  fontWeight: "bold",
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  color: `${blue}`,
+};
+
+function NewPost() {
+  return (
+    <Link to={"/"} style={buttonStyles}>
+      New Post
+      <AddIcon style={{ color: `${blue}` }} />
+    </Link>
+  );
+}
+
+export default NewPost;
