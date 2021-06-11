0cd62bb9a6ad8c559959de756250c17082a34030
OJailson17 <JaylsonO17@gmail.com>
2021-06-11 02:21:49 -0300

    adding dark mode feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import storage from "local-storage-fallback";
+
+import Login from "./pages/Auth/Login/Login";
+import Signup from "./pages/Auth/Signup/Signup";
+import Home from "./pages/Home/Home";
+import DesktopHeader from "./components/Header/DesktopHeader";
+import MobileHeader from "./components/Header/MobileHeader";
+
+import { useWindowDimensions } from "./Helper/windowDimension";
+
+import "./App.css";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) => props.theme.mode === "Dark" ? "#111" : "#eee"};
+    color: ${(props) => (props.theme.mode === "Dark" ? "#eee" : "#111")}
+  }
+  
+  header, nav {
+    background-color: ${(props) =>
+      props.theme.mode === "Dark" ? "#0821d4" : ""}
+  }
+`;
+
+const getInitialTheme = () => {
+  const savedTheme = storage.getItem("theme");
+  return savedTheme ? JSON.parse(savedTheme) : { mode: "Light" };
+};
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(true);
+  const [theme, setTheme] = useState(getInitialTheme);
+  const { width } = useWindowDimensions();
+
+  useEffect(() => {
+    storage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {width >= 768 ? (
+          <DesktopHeader
+            authenticated={authenticated}
+            theme={theme}
+            setTheme={setTheme}
+          />
+        ) : (
+          <MobileHeader
+            authenticated={authenticated}
+            theme={theme}
+            setTheme={setTheme}
+          />
+        )}
+        <div className="App">
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <Home theme={theme} setTheme={setTheme} />
+              </Route>
+
+              <Route exact path="/signup">
+                <Signup theme={theme} />
+              </Route>
+
+              <Route exact path="/login">
+                <Login theme={theme} />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
