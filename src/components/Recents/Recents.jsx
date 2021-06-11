ef1e49b6d7f44c5576a070dda5fbb1b3e709216b
OJailson17 <JaylsonO17@gmail.com>
2021-06-11 02:18:39 -0300

    create recentes component

---
+++ src/components/Recents/Recents.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import NewPost from "../NewPost/NewPost";
+
+import "./Recents.css";
+
+const borderStyle = {
+  border: "1.5px solid #fff",
+};
+
+function Recents({ theme }) {
+  const postArr = [
+    {
+      id: 1234,
+      title: "My first post",
+      body: "Some Random Text here",
+      category: "English",
+    },
+    {
+      id: 12345,
+      title: "My second post",
+      body: "Some Random Text here",
+      category: "Programming",
+    },
+    {
+      id: 123456,
+      title: "My third post",
+      body: "Some Random Text here",
+      category: "English",
+    },
+  ];
+  return (
+    <section className="recents-container">
+      <div className="section-title">
+        <h3>Recents</h3>
+        <NewPost />
+      </div>
+
+      <div className="posts-wrapper">
+        {postArr.map((element) => (
+          <div className="posts-container recent " key={element.id}>
+            <div className="post-title">
+              <span>
+                <strong>{element.title}</strong>
+              </span>
+            </div>
+            <div className="post-info">
+              <span className="category-name">
+                Categorie - <strong>{element.category}</strong>
+              </span>
+              <span className="post-date">29/05/2021 - 07:50</span>
+            </div>
+            <div className="post">
+              <div style={theme.mode === "Dark" ? borderStyle : {}}>
+                <p>{element.body}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Recents;
