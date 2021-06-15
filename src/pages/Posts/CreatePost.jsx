252308a71154c16c2a4199e6396c2e2ac881c608
OJailson17 <JaylsonO17@gmail.com>
2021-06-14 23:17:55 -0300

    new post page added

---
+++ src/pages/Posts/CreatePost.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import PostForm from '../../components/PostForm/PostForm'
+import Footer from '../../components/Footer/Footer'
+import { dataContext } from '../../Context'
+
+
+const titleStyle = {
+    textAlign: "center",
+    marginTop: "32px"
+}
+
+function CreatePost() {
+  const {theme} = useContext(dataContext)
+
+    return (
+        <>
+        <section className="new-post-container">
+        <div className="new-post-title" style={titleStyle}>
+            <h1>Create Post</h1>
+        </div>
+        <PostForm btnText="Create"/>
+        </section>
+        <Footer theme={theme} />
+        </>
+    )
+}
+
+export default CreatePost
