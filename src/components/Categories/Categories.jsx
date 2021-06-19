4c671c45e4feb532cec81dceb9f855371124bae8
OJailson17 <JaylsonO17@gmail.com>
2021-06-19 13:59:40 -0300

    changing Link component to a anchor

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
 import englishImage from "../../images/english-image.jpg";
 import programmingImage from "../../images/programming.jpeg";
 
@@ -17,19 +15,19 @@ function Categories({ theme }) {
         <h3>Categories</h3>
       </div>
       <div className="categories-wrapper">
-        <Link to={"/posts/English"} style={theme.mode === "Dark" ? linkStyles : {}}>
+        <a href="/posts/English" style={theme.mode === "Dark" ? linkStyles : {}}>
           <div className="english-categorie category">
             <span>English</span>
             <img loading="lazy" src={englishImage} alt="" />
           </div>
-        </Link>
+        </a>
 
-        <Link to={"/posts/Programming"} style={theme.mode === "Dark" ? linkStyles : {}}>
+        <a href="/posts/Programming" style={theme.mode === "Dark" ? linkStyles : {}}>
           <div className="programming-categorie category">
             <span>Programming</span>
             <img loading="lazy" src={programmingImage} alt="" />
           </div>
-        </Link>
+        </a>
       </div>
     </section>
   );
