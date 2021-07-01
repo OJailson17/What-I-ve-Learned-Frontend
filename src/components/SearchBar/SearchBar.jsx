26d5ee01eaf4c356a655e05fb4d3d1b688f293ba
OJailson17 <JaylsonO17@gmail.com>
2021-07-01 00:11:19 -0300

    adding all search post feature

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { dataContext } from "../../Context";
 
 import "./SearchBar.css";
 
@@ -13,14 +14,33 @@ const btnStyle = {
 };
 
 function SearchBar({ theme }) {
+  const [searchInput, setSearchInput] = useState('')
+  const [foundPosts, setFoundPosts] = useState('')
+  const {allPosts} = useContext(dataContext)
+  // console.log(allPosts);
+
+  const searchPost = (event) => {
+    event.preventDefault()
+
+    allPosts.map(post => {
+      if(post?.title.indexOf(searchInput) !== -1){
+        console.log(post);
+      } else if (post?.body.indexOf(searchInput) !== -1) {
+        console.log(post);
+      }
+    })
+  }
+
   return (
     <section className="searchbar-container">
-      <form className="search-form">
+      <form className="search-form" onSubmit={searchPost}>
         <input
           type="text"
           className="search-input"
           autoComplete="off"
           placeholder="Seach for a topic"
+          onChange={e => setSearchInput(e.target.value)}
+          value={searchInput}
           style={theme.mode === "Dark" ? inputStyle : {}}
         />
         <button style={theme.mode === "Dark" ? btnStyle : {}}>Search</button>
