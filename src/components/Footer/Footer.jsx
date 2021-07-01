9a1f77ed0592ac1321a78646b20b22954777975e
OJailson17 <JaylsonO17@gmail.com>
2021-07-01 01:59:20 -0300

    updating footer social media links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,22 +11,22 @@ function Footer({ theme }) {
     <footer style={theme.mode === "Dark" ? footerDarkStyle : {}}>
       <div className="social-media-container">
         <div className="social-media">
-          <a target="_blank" rel="noopener" href="/">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/jaylson.deoliveira">
             <i className="fab fa-facebook-f"></i>
           </a>
         </div>
         <div className="social-media">
-          <a target="_blank" rel="noopener" href="/">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/jaylsonolliveira/">
             <i className="fab fa-instagram"></i>
           </a>
         </div>
         <div className="social-media">
-          <a target="_blank" rel="noopener" href="/">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/OJailson17">
             <i className="fab fa-github"></i>
           </a>
         </div>
         <div className="social-media">
-          <a target="_blank" rel="noopener" href="/">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jailson-de-oliveira-674951212/">
             <i className="fab fa-linkedin-in"></i>
           </a>
         </div>
