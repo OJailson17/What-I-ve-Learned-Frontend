814e94273c16c30cc5414b38888a1bab977dcdd9
OJailson17 <JaylsonO17@gmail.com>
2021-07-03 23:56:55 -0300

    adding Context Providers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ApplicationProvider } from "./Context";
+import { ApplicationProvider } from "./Context/ApplicationContext";
+import {ThemeProvider} from './Context/ThemeContext'
+import {AuthProvider} from './Context/AuthContext'
+import {UserProvider} from './Context/UserContext'
 
 
 ReactDOM.render(
     <ApplicationProvider>
+    <AuthProvider>
+    <UserProvider>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
+    </UserProvider>
+    </AuthProvider>
     </ApplicationProvider>
 , document.getElementById("root"));
